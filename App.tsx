88d55ed06b05a60d1fa13bf5c35c6d87cbb7348c
rahul singh <rahul.libra19@gmail.com>
2024-04-11 11:13:21 +0530

    send code
    send code

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { View, StyleSheet } from 'react-native';
+import NavigationComponent from './src/navigation/index'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+
+const queryClient = new QueryClient()
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NavigationComponent />
+    </QueryClientProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  }
+});
